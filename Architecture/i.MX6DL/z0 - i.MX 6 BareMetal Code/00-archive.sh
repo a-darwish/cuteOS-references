c8d95601d7eb26e3ca549f3edb11920e3c00ae74
Ahmed S. Darwish <darwish.07@gmail.com>
2015-06-01 12:03:56 +0300

    Architecture: Add references for the i.MX 6DualLite platform.
    1. Fact sheet provides a nice global view of all i.MX 6 processors
    2. Data sheet provides a nice block diagram for i.MX 6DualLite
    3. Reference manual provides all the nitty-gritty details of every
       core in the i.MX 6DualLite in a 6000-page document
    4. Firmware development guide will be necessary for our board
       bring-up efforts
    5. Bare Metal guide will be quite nice for our minimal bootloader
    6. Platfrom SDK will be necessary for all the SoC pin, multiplexing,
       and padding definitions

---
+++ Architecture/i.MX6DL/z0 - i.MX 6 BareMetal Code/00-archive.sh
@@ -0,0 +1,10 @@
+wget \
+     --recursive \
+     --no-clobber \
+     --page-requisites \
+     --html-extension \
+     --convert-links \
+     --restrict-file-names=windows \
+     --domains atose.org \
+     --no-parent \
+	'http://atose.org'
